56dc0aa7548232be12cb319995fbab0c0854feb9
Chen Zhao Xing <chenzhaoxing.98@gmail.com>
2022-09-30 20:40:56 +0800

    feat - added api-gateway to route api calls by the user

--- carbonO/carbonTracker/src/main/java/com/carbonO/CarbonOCarbonTrackerApplication.java
+++ carbonO/carbonTracker/src/main/java/com/carbonO/CarbonOCarbonTrackerApplication.java
@@ -1,7 +1,6 @@
 package com.carbonO;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
