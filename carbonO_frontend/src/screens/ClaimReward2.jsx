458a9275ce60d2a3d477cd4b2fa62ad60578121c
Novia Antony <noviaantony2001@gmail.com>
2022-09-30 20:33:46 +0800

    bug: Fix bug for converting image file to base64 (for Cloud Vision API POST request)

--- carbonO_frontend/src/screens/ClaimReward2.jsx
+++ carbonO_frontend/src/screens/ClaimReward2.jsx
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import FileUploaded from "./FileUploader";
-const ClaimReward2 = () => {
 
+
+const ClaimReward2 = () => {
   const [selectedFile, setSelectedFile] = useState(null);
 
   const submitReceipt = () => {
@@ -17,41 +18,68 @@ const ClaimReward2 = () => {
 
     encodeImageURI(selectedFile)
       .then((base64) => {
+
+        // let body = {
+        //   requests: [
+        //     {
+        //       image: {
+        //         source: {
+        //           imageUri: encodeImageURI(selectedFile),
+        //           // "https://p2d7x8x2.stackpathcdn.com/content/uploads/2016/05/Bill.jpg", //image URL
+        //         },
+        //       },
+        //       features: [
+        //         {
+        //           type: "TEXT_DETECTION",
+        //           maxResults: 1,
+        //         },
+        //       ],
+        //     },
+        //   ],
+        // };
+
         console.log(base64);
-        // Do your stuff
+        console.log(base64.substring(23, base64.length));
+
+        let body = {
+          requests: [
+            {
+              image: {
+                content: base64.substring(23, base64.length),
+              },
+              features: [
+                {
+                  type: "TEXT_DETECTION",
+                },
+              ],
+            },
+          ],
+        };
+
+        axios
+          .post(
+            "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyC3rJnOOPzFe0snLWmBrgukHmSqFxlimU8",
+            body
+          )
+          .then((response) => 
+            // console.log(response)
+            console.log(response.data.responses[0].textAnnotations)
+          );
+
+          
+
+          
+
+
       })
       .catch((error) => {
         console.error(error);
         // Handle your error
       });
 
-    let body = {
-      requests: [
-        {
-          image: {
-            source: {
-              imageUri: encodeImageURI(selectedFile),
-              // "https://p2d7x8x2.stackpathcdn.com/content/uploads/2016/05/Bill.jpg", //image URL
-            },
-          },
-          features: [
-            {
-              type: "TEXT_DETECTION",
-              maxResults: 1,
-            },
-          ],
-        },
-      ],
-    };
 
-    axios
-      .post(
-        "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyC3rJnOOPzFe0snLWmBrgukHmSqFxlimU8",
-        body
-      )
-      .then((response) => console.log(response));
+
   };
-
 
   return (
     <div>
@@ -64,7 +92,8 @@ const ClaimReward2 = () => {
       <button onClick={submitReceipt}>Submit</button>
     </div>
   );
-}
-
+};
 
 export default ClaimReward2;
+
+
