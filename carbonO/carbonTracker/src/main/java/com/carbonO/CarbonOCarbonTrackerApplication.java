ac77435a2ee6ac5c9b071bba44977655dfcb30b2
Chen Zhao Xing <chenzhaoxing.98@gmail.com>
2022-10-01 13:06:11 +0800

    feat - Added eureka server and api gateway to allow microservices to communication with each other

--- carbonO/carbonTracker/src/main/java/com/carbonO/CarbonOCarbonTrackerApplication.java
+++ carbonO/carbonTracker/src/main/java/com/carbonO/CarbonOCarbonTrackerApplication.java
@@ -2,9 +2,10 @@ package com.carbonO;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
+@EnableEurekaClient
 public class CarbonOCarbonTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarbonOCarbonTrackerApplication.class, args);
