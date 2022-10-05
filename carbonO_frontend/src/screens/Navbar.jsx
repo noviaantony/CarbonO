dbda60ad727f24fb1eda1dafb5ba512c0227f8f8
Alison How <alison.how.2021@scis.smu.edu.sg>
2022-10-06 03:24:43 +0800

    fea- added logout button feat

--- carbonO_frontend/src/screens/Navbar.jsx
+++ carbonO_frontend/src/screens/Navbar.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import Logo from "../assets/images/LogoPng.png";
 
 const Navbar = () => {
-
-
+  const [showModal, setShowModal] = React.useState(false);
 
   return (
     <nav className="bg-[#5E9387] px-2 sm:px-4 py-2.5 w-full z-20 sticky top-0 left-0 font-default">
@@ -20,9 +19,57 @@ const Navbar = () => {
             className="bg-primary-500 text-[#5E9387]  focus:bg-primary-700 focus:text-gray-200 focus:shadow-outline focus:outline-none transition duration-300
             px-7 py-2 w-full bg-white text-center rounded-md block sm:w-auto font-bold hover:bg-gray-700 hover:text-white"
             as="a"
+            type="button"
+            onClick={() => setShowModal(true)}
           >
             Log Out
           </button>
+
+          {/* more info modal */}
+          {showModal ? (
+            <>
+              <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                  {/*content*/}
+                  <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    {/*header*/}
+                    <div className="flex items-start justify-between text-center p-3 border-b border-solid border-slate-200 rounded-t">
+                      <h3 className="text-2xl font-semibold">
+                        Are you sure you want to log out?
+                      </h3>
+                      <button
+                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                        onClick={() => setShowModal(false)}
+                      >
+                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                          ×
+                        </span>
+                      </button>
+                    </div>
+                    {/*footer*/}
+                    <div className="flex items-center justify-end p-3 border-t border-solid border-slate-200 rounded-b">
+                      <button
+                        className="justify-center inline-flex items-center py-5 px-5 text-xl font-xs text-center text-white bg-[#5E9387] rounded-lg  focus:outline-none transition duration-300 mr-3 font-semibold hover:bg-gray-700 hover:text-white"
+                        type="button"
+                        onClick={() => setShowModal(false)}
+                      >
+                        Yes
+                      </button>
+                      <button
+                      type="button"
+                      class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                      data-bs-dismiss="modal" aria-label="Close"
+                      >
+                        
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            </>
+          ) : null}
+
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
@@ -89,6 +136,6 @@ const Navbar = () => {
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
