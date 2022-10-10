e5b6caea2526b2d5679fab69db306e24541577fe
Alison How <alison.how.2021@scis.smu.edu.sg>
2022-10-10 17:26:06 +0800

    chore: update and sync all changes

--- carbonO_frontend/src/screens/Dashboard.jsx
+++ carbonO_frontend/src/screens/Dashboard.jsx
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
 //import Wallet from "../components/dashboard/Wallet";
 import Table from "../components/dashboard/Table";
 //import DonutChart from "../components/dashboard/DonutChart";
-import Statistics from "../components/dashboard/Statistics";
-=======
-import React, {useContext, useEffect, useState} from "react";
-import Wallet from "../components/dashboard/Wallet";
-import Table from "../components/dashboard/Table";
-import DonutChart from "../components/dashboard/DonutChart";
 import UserStatistics from "../components/dashboard/UserStatistics";
+import { useState, useEffect, useContext } from "react";
 import AuthContext from "../context/AuthProvider";
 import carbonTrackerService from "../services/CarbonTrackerService";
 
-
->>>>>>> 302c8079b40f20e4d1090189c7b65366a10f5cda
 
 const Dashboard = () => {
   const [historicalData, setHistoricalData] = useState([]);
@@ -44,21 +36,13 @@ const Dashboard = () => {
       <div>
         <h1 class="text-2xl ml-6 mt-4 font-bold">Welcome Back, {auth.firstName}!</h1>
         {/* <Wallet /> */}
-<<<<<<< HEAD
-        <Statistics />
-        <Table />
+        <UserStatistics TotalCarbon={totalCarbon.toFixed(0)}/>
+        <Table historicalData={historicalData}/>
         {/* <div class="flex flex-wrap -mx-1 lg:-mx-4">
           <DonutChart />
           <DonutChart />
           <DonutChart />
         </div> */}
-=======
-        <UserStatistics TotalCarbon={totalCarbon.toFixed(0)}/>
-        <Table historicalData={historicalData}/>
-        <div class="flex flex-wrap -mx-1 lg:-mx-4">
-          {/*<DonutChart />*/}
-        </div>
->>>>>>> 302c8079b40f20e4d1090189c7b65366a10f5cda
       </div>
     </>
   );
