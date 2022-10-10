06560a368e6978ec2ee7a92880479e984de51b95
Alison How <alison.how.2021@scis.smu.edu.sg>
2022-10-10 16:58:12 +0800

    chore: make info boxes in dashboard clickable

--- carbonO_frontend/src/screens/Dashboard.jsx
+++ carbonO_frontend/src/screens/Dashboard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Wallet from "../components/dashboard/Wallet";
+//import Wallet from "../components/dashboard/Wallet";
 import Table from "../components/dashboard/Table";
-import DonutChart from "../components/dashboard/DonutChart";
-import Testing from "../components/dashboard/Testing";
+//import DonutChart from "../components/dashboard/DonutChart";
+import Statistics from "../components/dashboard/Statistics";
 
 const Dashboard = () => {
   return (
@@ -10,13 +10,13 @@ const Dashboard = () => {
       <div>
         <h1 class="text-2xl ml-6 mt-4 font-bold">Welcome Back User</h1>
         {/* <Wallet /> */}
-        <Testing />
+        <Statistics />
         <Table />
-        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+        {/* <div class="flex flex-wrap -mx-1 lg:-mx-4">
           <DonutChart />
           <DonutChart />
           <DonutChart />
-        </div>
+        </div> */}
       </div>
     </>
   );
