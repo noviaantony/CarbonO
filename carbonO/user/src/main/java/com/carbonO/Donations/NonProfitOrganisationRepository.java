5fd8b077e2e3b39b683aef16f391869dc8e99023
Chen Zhao Xing <chenzhaoxing.98@gmail.com>
2022-11-06 01:08:01 +0800

    feat: Added donation entity to backend

---
+++ carbonO/user/src/main/java/com/carbonO/Donations/NonProfitOrganisationRepository.java
@@ -0,0 +1,12 @@
+package com.carbonO.Donations;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NonProfitOrganisationRepository extends JpaRepository<NonProfitOrganisation, Long> {
+    List<NonProfitOrganisation> findAllByOrderByOrganisationNameAsc();
+}
+
--- carbonO/user/src/main/java/com/carbonO/User/UserConfig.java
+++ carbonO/user/src/main/java/com/carbonO/User/UserConfig.java
@@ -1,5 +1,7 @@
 package com.carbonO.User;
 
+import com.carbonO.Donations.NonProfitOrganisation;
+import com.carbonO.Donations.NonProfitOrganisationService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +13,11 @@ import java.util.List;
 public class UserConfig {
     //temp encoder to create an encrypted password
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final NonProfitOrganisationService nonProfitOrganisationService;
 
-    public UserConfig(BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserConfig(BCryptPasswordEncoder bCryptPasswordEncoder, NonProfitOrganisationService nonProfitOrganisationService) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.nonProfitOrganisationService = nonProfitOrganisationService;
     }
 
     @Bean
@@ -40,6 +44,38 @@ public class UserConfig {
             repository.saveAll(
                     List.of(xz,tester)
             );
+
+            List<NonProfitOrganisation> nonProfitOrganisationsList = List.of(
+                    new NonProfitOrganisation(
+                            "Cool Earth: Protecting the World’s Rainforests",
+                            "Enact federal policy that would force older coal plants in America to improve their emissions. Today, the charity is a global non-profit organization that works to safeguard against the worst impacts of climate change.",
+                            "https://856366.smushcdn.com/2556047/wp-content/uploads/2022/09/AdobeStock__307657351-1024x683.jpeg?lossy=0&strip=1&webp=1"
+                    ),
+                    new NonProfitOrganisation(
+                            "Clean Air Task Force: Reducing Carbon Emissions",
+                            "Charity is dedicated to addressing climate change through the protection of rainforests, by giving control back to the people who live in them.",
+                            "https://as2.ftcdn.net/v2/jpg/03/73/58/69/1000_F_373586987_Cbsttxh4guQm9RJDgDFa1kw8L2XiF9EI.jpg"
+                    ),
+                    new NonProfitOrganisation(
+                            "Depave: Create More Green Spaces For All",
+                            "A driven organisation that seeks to transition to a regenerative argriculture through the focus of sustainable farms and grasslands.",
+                            "https://as2.ftcdn.net/v2/jpg/05/12/41/73/1000_F_512417375_Q8quk5IiZE84fpnaaRGqBfh76gqDpCDZ.jpg"
+                    ),
+                    new NonProfitOrganisation(
+                            "WattTime: Leveraging Technology to Save the Environment",
+                            "This organisation designs Automated Emissions Reduction solutions to empower all users to choose clean energy.",
+                            "https://as1.ftcdn.net/v2/jpg/04/71/77/90/1000_F_471779082_mHHJIPaFThAkAP7k7YBrBJ4uH48LmbGq.jpg"
+                    ),
+                    new NonProfitOrganisation(
+                            "The Carbon Underground: An Agricultural Combat Against Climate Change",
+                            "a driven organisation that seeks to transition to a regenerative agriculture through the focus of sustainable farms and grasslands",
+                            "https://as2.ftcdn.net/v2/jpg/05/12/41/73/1000_F_512417375_Q8quk5IiZE84fpnaaRGqBfh76gqDpCDZ.jpg"
+                    )
+            );
+
+            for (NonProfitOrganisation nonProfitOrganisation : nonProfitOrganisationsList) {
+                nonProfitOrganisationService.addNonProfitOrganisation(nonProfitOrganisation);
+            }
         };
     }
 }
