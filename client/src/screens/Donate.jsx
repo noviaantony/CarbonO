57c1704569fbf267b99429bcad4e4677b07a1b91
Alison How <alison.how.2021@scis.smu.edu.sg>
2022-11-05 18:58:14 +0800

    create: add new donation companies

--- client/src/screens/Donate.jsx
+++ client/src/screens/Donate.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
 import Header from "../components/misc/Header";
 import DonationCard from "../components/donate/DonationCard";
-
-
+import GreenSpace from "./GreenSpace.jpeg";
+import CleanAir from "./CleanAir.jpeg";
+import Technology from "./Technology.jpeg";
+import Agriculture from "./Agriculture.jpeg";
 const Donate = () => {
-
   return (
     <>
       <div className="h-max">
@@ -13,7 +14,7 @@ const Donate = () => {
           Description="use the e-credits you earned to donate to climate change campaigns, we make donating easy for you."
         />
 
-        <div class="container my-12 mx-auto px-4 md:px-1 h-screen">
+        <div class="container my-12 mx-auto px-4 md:px-1">
           <div class="flex flex-wrap -mx-1 lg:-mx-4">
             <DonationCard
               Title="Cool Earth: Protecting the World’s Rainforests"
@@ -25,14 +26,35 @@ const Donate = () => {
             <DonationCard
               Title="Clean Air Task Force: Reducing Carbon Emissions"
               Description="charity is dedicated to addressing climate change through the protection of rainforests, by giving control back to the people who live in them"
-              Image="https://856366.smushcdn.com/2556047/wp-content/uploads/2022/09/AdobeStock__307657351-1024x683.jpeg?lossy=0&strip=1&webp=1"
+              Image={CleanAir}
               Website="https://www.catf.us/"
+            />
+
+            <DonationCard
+              Title="Depave: Create More Green Spaces For All"
+              Description="this charity aims to create more urban green spaces and free the environment from any injustice it has suffered "
+              Image={GreenSpace}
+              Website="https://depave.org/"
+            />
+
+            <DonationCard
+              Title="The Carbon Underground: An Agricultural Combat Against Climate Change"
+              Description="a driven organisation that seeks to transition to a regenerative argriculture through the focus of sustainable farms and grasslands "
+              Image={Agriculture}
+              Website="https://thecarbonunderground.org/"
+            />
+
+            <DonationCard
+              Title="WattTime: Leveraging Technology to Save the Environment"
+              Description="this organisation designs Automated Emissions Reduction solutions to empower all users to choose clean energy"
+              Image={Technology}
+              Website="https://www.watttime.org/"
             />
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
-export default Donate
+export default Donate;
