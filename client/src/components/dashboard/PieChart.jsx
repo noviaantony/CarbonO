12d5175c0a57e7cb9dce2cfa6d7e9914a279640b
Alison How <alison.how.2021@scis.smu.edu.sg>
2022-11-07 01:57:47 +0800

    fix: created object array to pass info to charts

--- client/src/components/dashboard/PieChart.jsx
+++ client/src/components/dashboard/PieChart.jsx
@@ -1,19 +1,36 @@
-import React, {useState, useEffect, useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Pie } from "react-chartjs-2";
-import 'chart.js/auto';
-import { Chart, LineController, LineElement, PointElement, LinearScale, Title,CategoryScale } from 'chart.js';
+import "chart.js/auto";
+import {
+  Chart,
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale,
+} from "chart.js";
 import CarbonTrackerService from "../../services/CarbonTrackerService";
 import AuthContext from "../../hooks/AuthProvider";
 
-Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
+Chart.register(
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale
+);
 
-Chart.defaults.font.size = 8;
+Chart.defaults.font.size = 16;
 
 // carbon rating summary
 
 const PieChart = () => {
-
   const [consumptionData, setConsumptionData] = useState([]);
+  const [chartsData, setChartsData] = useState([
+    { date: "", totalCarbonRating: 0 },
+  ]);
 
   const { auth, setAuth } = useContext(AuthContext);
 
@@ -24,38 +41,67 @@ const PieChart = () => {
         console.log(response);
         setConsumptionData(response);
         console.log(consumptionData);
-      //  setLoading(false);
+        //  setLoading(false);
       }
     );
   }, []);
 
   console.log("--", consumptionData);
 
-  const chartsData = [];
+  // const handleChange = (input) => {
+  //   console.log("here")
+  //   setChartsData(
+  //     chartsData.map((data) => {
+  //       if (data.date === input.dateConsumed.substring(0, 10)) {
+  //         return {
+  //           ...data,
+  //           date: input.dateConsumed.substring(0, 10),
+  //           totalCarbonRating: data.totalCarbonRating + input.pointsEarned,
+  //         };
+  //       } else {
+  //         return {
+  //           ...data,
+  //           date: input.dateConsumed.substring(0, 10),
+  //           totalCarbonRating: input.pointsEarned,
+  //         };
+  //       }
+  //     })
+  //   );
+  // };
 
-  for (let i = 0; consumptionData.length; i++) {
-
-
-    if (chartsData.filter((data) => data.date === consumptionData[i].dateConsumed.substring(0, 10))) {
-      chartsData.push({
-        date: consumptionData[i].dateConsumed.substring(0, 10),
-        totalCarbonRating: consumptionData[i].totalCarbonRating + data.totalCarbonRating,
-      });
-    } else {
-      chartsData.push({
-        date: consumptionData[i].dateConsumed.substring(0, 10),
-        totalCarbonRating: consumptionData[i].totalCarbonRating,
-      });
-    }     
-  }
-
-  console.log(chartsData);
+  const test = () => {
+    let i = 0;
+      useEffect(() => {
+        for (i; i < consumptionData.length; i++) {
+        setChartsData(
+          chartsData.map((data) => {
+            if (data.date === consumptionData[i].dateConsumed.substring(0, 10)) {
+              console.log("here1")
+              return {
+                ...data,
+                date: consumptionData[i].dateConsumed.substring(0, 10),
+                totalCarbonRating: data.totalCarbonRating + consumptionData[i].pointsEarned,
+              };
+            } else {
+              console.log("here2")
+              return {
+                ...data,
+                date: consumptionData[i].dateConsumed.substring(0, 10),
+                totalCarbonRating: consumptionData[i].pointsEarned,
+              };
+            }
+          })
+        );
+      }},[consumptionData[i]]); 
+      console.log(chartsData);
+}
 
   return (
     <div
       style={{ width: "28%", height: "30%" }}
       className="bg-white rounded-lg h-auto p-6 flex items-stretch m-6 font-default"
     >
+      {test()}
       <Pie
         data={{
           //labels on x-axis
@@ -63,7 +109,7 @@ const PieChart = () => {
           datasets: [
             {
               label: "Receipts",
-              data: [12, 19, 3, 6,8],
+              data: [12, 19, 3, 6, 8],
               backgroundColor: [
                 "rgba(255, 99, 132, 0.2)",
                 "rgba(54, 162, 235, 0.2)",
