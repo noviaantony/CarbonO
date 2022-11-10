79c362cc83dd6bf54057139ef9d2325eb3d3ab2e
Chen Zhao Xing <chenzhaoxing.98@gmail.com>
2022-11-11 02:09:23 +0800

    feat: Added spring security to user reward microservice

---
+++ carbonO/userReward/src/main/java/com/carbonO/Security/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.carbonO.Security.config;
+
+import com.carbonO.Security.CustomAuthorizationFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+@Order(2)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        CustomAuthorizationFilter customAuthorizationFilter = new CustomAuthorizationFilter();
+        http.cors().and().csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+//        http.authorizeRequests().antMatchers("/api/v1/carbonO/**").authenticated();
+//        http.authorizeRequests().antMatchers("/api/v1/carbonO/**").hasAnyAuthority("ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+//        http.addFilterBefore(customAuthorizationFilter);
+    }
+}
