d8f5a39c28abd3f90d94d083f364bba3604e97a0
chongyuansheng <chongyuansheng@gmail.com>
2022-11-11 01:31:58 +0800

    Chore: Included whitelisting

--- carbonO/user/src/main/java/com/carbonO/User/User.java
+++ carbonO/user/src/main/java/com/carbonO/User/User.java
@@ -3,6 +3,7 @@ package com.carbonO.User;
 
 import com.carbonO.Registration.token.ConfirmationToken;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.sun.istack.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,7 +17,7 @@ import java.util.Collections;
 import java.util.List;
 
 @Entity
-@Table (name = "users")
+@Table(name = "users")
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -33,9 +34,16 @@ public class User implements UserDetails {
             generator = "user_sequence"
     )
     private Long id;
+
+    @NotNull
     private String firstName;
+
+    @NotNull
     private String lastName;
+
+    @NotNull
     private String email;
+    @NotNull
     private String password;
 
     @Enumerated(EnumType.STRING)
@@ -45,7 +53,7 @@ public class User implements UserDetails {
 
     private Boolean enabled = false; //temporarily set to true
 
-    @OneToMany (mappedBy = "user", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonManagedReference
     private List<ConfirmationToken> confirmationToken;
 
@@ -63,6 +71,7 @@ public class User implements UserDetails {
         this.password = password;
         this.userRole = userRole;
     }
+
     public User(String firstName, String lastName, String email, String password, UserRole userRole, Boolean enabled) {
         this.firstName = firstName;
         this.lastName = lastName;
